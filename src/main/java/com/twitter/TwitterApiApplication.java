4c9cb76e5b5c64892930fdc07fcb63036e5ec76b
Vasim Akram <iamvasim786@gmail.com>
2016-05-12 12:04:16 +0530

    commit

---
+++ src/main/java/com/twitter/TwitterApiApplication.java
@@ -0,0 +1,12 @@
+package com.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApiApplication.class, args);
+	}
+}
